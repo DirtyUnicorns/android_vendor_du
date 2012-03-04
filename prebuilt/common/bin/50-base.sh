69c632c67ebf5a03ff6302431b38ae58db469232
Warren Togami <wtogami@gmail.com>
2012-03-03 22:37:42 -1000

    Modular backuptool.sh
    Tips & Tricks
    =============
    * 50-base.sh contains only a reference implementation.  You may customize the methods however you wish.
      For example, 20-foobar.sh pre-backup can use a loop with conditionals to generate a dynamic backup list in
      /tmp/foobar_file_list which is later printed by list_files() so the backup method will act on those files.
    * Optional methods pre-backup, post-backup, pre-restore, or post-restore may be defined for special purposes.
    * Inject new files into /tmp/addon.d/ prior to backuptool.sh backup if you want to act during the current system upgrade.
    * Delete files from /tmp/addon.d/ during post-restore if you want to permanently remove files from /system/addon.d/
      Addons may use this approach to run a script only once.
    * Scripts run in sort -n order.  Prefix with numbers 00 through 99 if want to run in a particular order.
    * You can have two separate scripts, implementing only backup in one, and only restore in the other with a different
      number prefix of each.  This allows even greater control the backup/restore order even further.
    * You could use pre-backup to generate a one-time use backup script in /tmp/addon.d/ that deletes itself in
      post-restore.
    
    Conflicts:
    	prebuilt/common/apk/Android.mk
    
    Change-Id: Idb4157f5b94d019cec7a0f9e402c96e3192708c5

--- prebuilt/common/bin/50-base.sh
+++ prebuilt/common/bin/50-base.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 # 
-# /system/addon.d/50-du.sh
-# During an upgrade, this script backs up /system/etc/hosts,
+# /system/addon.d/50-base.sh
+# During a upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
 #
 
