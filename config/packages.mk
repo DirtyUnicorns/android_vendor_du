309b1a68bf58bd74a42019f1146b59a3eb5d6b55
Alex <mazdarider23@gmail.com>
2013-12-13 15:10:21 -0600

    Remove these packages...

--- config/packages.mk
+++ config/packages.mk
@@ -14,8 +14,7 @@ PRODUCT_PACKAGES += \
 	DSPManager \
 	libcyanogen-dsp \
 	audio_effects.conf \
-	MonthCalendarWidget \
-	OpenDelta
+	MonthCalendarWidget
 
 PRODUCT_PACKAGES += \
 	CellBroadcastReceiver
