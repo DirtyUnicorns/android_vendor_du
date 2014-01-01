a9e725a9036e64e1f418340d878b184b37798b9e
barry-smithjr <barry.smithjr@gmail.com>
2013-12-31 18:42:54 -0600

    vendor: OCD moment, order devices by name
    Change-Id: I754305c4e0e2874e9d65774cfde7296df23f2369

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,9 @@
+add_lunch_combo du_exhilarate-userdebug
 add_lunch_combo du_flo-userdebug
+add_lunch_combo du_grouper-userdebug
 add_lunch_combo du_hammerhead-userdebug
 add_lunch_combo du_m7att-userdebug
+add_lunch_combo du_m7spr-userdebug
 add_lunch_combo du_m7ul-userdebug
 add_lunch_combo du_mako-userdebug
-add_lunch_combo du_exhilarate-userdebug
 add_lunch_combo du_t769-userdebug
-add_lunch_combo du_grouper-userdebug
-add_lunch_combo du_m7spr-userdebug
-
