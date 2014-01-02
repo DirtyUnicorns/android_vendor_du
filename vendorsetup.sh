5908532018eb87968ca1472f921a74b34e78e7ec
barry-smithjr <barry.smithjr@gmail.com>
2014-01-02 17:33:29 -0600

    vendorsetup: Add i9300 and i9305

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,8 @@ add_lunch_combo du_exhilarate-userdebug
 add_lunch_combo du_flo-userdebug
 add_lunch_combo du_grouper-userdebug
 add_lunch_combo du_hammerhead-userdebug
+add_lunch_combo du_i9300-userdebug
+add_lunch_combo du_i9305-userdebug
 add_lunch_combo du_m7att-userdebug
 add_lunch_combo du_m7spr-userdebug
 add_lunch_combo du_m7ul-userdebug
