51a017d0774394c311dfa2ae1c68bd938d3aa4b8
bigdaddy92474@gmail.com <bigdaddy92474@gmail.com>
2014-01-03 20:06:02 -0600

    add devices
    Change-Id: I9bb2200143bf112395f4089dff9c7d121e45d741

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,3 +10,5 @@ add_lunch_combo du_m7ul-userdebug
 add_lunch_combo du_mako-userdebug
 add_lunch_combo du_p3110-userdebug
 add_lunch_combo du_t769-userdebug
+add_lunch_combo du_d710-userdebug
+add_lunch_combo du_toroplus-userdebug
