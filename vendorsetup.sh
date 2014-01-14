61e09d6f70d0adc62fe8edea3a40881a15455675
bigdaddy92474@gmail.com <bigdaddy92474@gmail.com>
2014-01-13 23:28:01 -0600

    add skyrocket
    Change-Id: Ifacb7a1c2d8bcf693afe39294a8fb9210929f7bb

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,3 +14,4 @@ add_lunch_combo du_manta-userdebug
 add_lunch_combo du_p3110-userdebug
 add_lunch_combo du_t769-userdebug
 add_lunch_combo du_toroplus-userdebug
+add_lunch_combo du_skyrocket-userdebug
