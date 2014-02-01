66ae9608917b959acd7c9572e37c48502032bde8
Alex Cruz <mazdarider23@gmail.com>
2014-02-01 17:33:42 -0600

    Up version number for official release
    Change-Id: I1a2c267368b059f28b70c5733ee928b573a3614a

--- config/common.mk
+++ config/common.mk
@@ -85,6 +85,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.romstats.url=http://stats.dirtyunicorns.com/ \
 ro.romstats.name=DirtyUnicorns \
-ro.romstats.version=5.0 \
+ro.romstats.version=6.0 \
 ro.romstats.tframe=3
 
