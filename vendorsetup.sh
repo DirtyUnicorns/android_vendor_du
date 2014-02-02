8215097908c2b912a100df1105217008ab0f72df
chasmodo <bosko.trkulja@gmail.com>
2014-02-02 10:18:06 +0100

    Vendor: add n7000 and yuga
    Change-Id: I2c3d933bace05db3db41e41b8878f4d5af67fd46

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,8 +14,10 @@ add_lunch_combo du_m7ul-userdebug
 add_lunch_combo du_maguro-userdebug
 add_lunch_combo du_mako-userdebug
 add_lunch_combo du_manta-userdebug
+add_lunch_combo du_n7000-userdebug
 add_lunch_combo du_p3110-userdebug
 add_lunch_combo du_t769-userdebug
 add_lunch_combo du_toroplus-userdebug
 add_lunch_combo du_skyrocket-userdebug
 add_lunch_combo du_quincyatt-userdebug
+add_lunch_combo du_yuga-userdebug
