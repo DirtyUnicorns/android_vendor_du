3eaae05327ad408c5187f12666d69c47f81b3a53
wantowan2 <wantowan2@gmail.com>
2014-02-13 04:00:53 -0500

    Added init.d Tweaks to common.mk
    Went ahead and added init.d tweaks that are already included in Dirty Tweaks
    
    Change-Id: I67ae8af6ad7a511567216339f7dd8b31438a6bb8

--- config/common.mk
+++ config/common.mk
@@ -29,21 +29,34 @@ PRODUCT_COPY_FILES += \
 
 # init.d support
 PRODUCT_COPY_FILES += \
-        vendor/du/prebuilt/bin/sysinit:system/bin/sysinit \
-        vendor/du/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
-        vendor/du/prebuilt/etc/init.d/00check:system/etc/init.d/00check \
-        vendor/du/prebuilt/etc/init.d/01zipalign:system/etc/init.d/01zipalign \
-        vendor/du/prebuilt/etc/init.d/02sysctl:system/etc/init.d/02sysctl \
-        vendor/du/prebuilt/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
-        vendor/du/prebuilt/etc/init.d/05freemem:system/etc/init.d/05freemem \
-        vendor/du/prebuilt/etc/init.d/06removecache:system/etc/init.d/06removecache \
-        vendor/du/prebuilt/etc/init.d/07fixperms:system/etc/init.d/07fixperms \
-        vendor/du/prebuilt/etc/init.d/09cron:system/etc/init.d/09cron \
-        vendor/du/prebuilt/etc/init.d/10sdboost:system/etc/init.d/10sdboost \
-        vendor/du/prebuilt/etc/init.d/98tweaks:system/etc/init.d/98tweaks \
-        vendor/du/prebuilt/etc/helpers.sh:system/etc/helpers.sh \
-        vendor/du/prebuilt/etc/sysctl.conf:system/etc/sysctl.conf \
-        vendor/du/prebuilt/etc/init.d.cfg:system/etc/init.d.cfg
+    vendor/du/prebuilt/bin/sysinit:system/bin/sysinit \
+    vendor/du/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/du/prebuilt/etc/init.d/00check:system/etc/init.d/00check \
+    vendor/du/prebuilt/etc/init.d/01zipalign:system/etc/init.d/01zipalign \
+    vendor/du/prebuilt/etc/init.d/02sysctl:system/etc/init.d/02sysctl \
+    vendor/du/prebuilt/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
+    vendor/du/prebuilt/etc/init.d/05freemem:system/etc/init.d/05freemem \
+    vendor/du/prebuilt/etc/init.d/06removecache:system/etc/init.d/06removecache \
+    vendor/du/prebuilt/etc/init.d/07fixperms:system/etc/init.d/07fixperms \
+    vendor/du/prebuilt/etc/init.d/09cron:system/etc/init.d/09cron \
+    vendor/du/prebuilt/etc/init.d/10sdboost:system/etc/init.d/10sdboost \
+    vendor/du/prebuilt/etc/init.d/11battery:system/etc/init.d/11battery \
+    vendor/du/prebuilt/etc/init.d/12touch:system/etc/init.d/12touch \
+    vendor/du/prebuilt/etc/init.d/13minfree:system/etc/init.d/13minfree \
+    vendor/du/prebuilt/etc/init.d/14gpurender:system/etc/init.d/14gpurender \
+    vendor/du/prebuilt/etc/init.d/15sleepers:system/etc/init.d/15sleepers \
+    vendor/du/prebuilt/etc/init.d/16journalism:system/etc/init.d/16journalism \
+    vendor/du/prebuilt/etc/init.d/17sqlite3:system/etc/init.d/17sqlite3 \
+    vendor/du/prebuilt/etc/init.d/18wifisleep:system/etc/init.d/18wifisleep \
+    vendor/du/prebuilt/etc/init.d/19iostats:system/etc/init.d/19iostats \
+    vendor/du/prebuilt/etc/init.d/20setrenice:system/etc/init.d/20setrenice \
+    vendor/du/prebuilt/etc/init.d/21tweaks:system/etc/init.d/21tweaks \
+    vendor/du/prebuilt/etc/init.d/24speedy_modified:system/etc/init.d/24speedy_modified \
+    vendor/du/prebuilt/etc/init.d/25loopy_smoothness_tweak:system/etc/init.d/25loopy_smoothness_tweak \
+    vendor/du/prebuilt/etc/init.d/98tweaks:system/etc/init.d/98tweaks \
+    vendor/du/prebuilt/etc/helpers.sh:system/etc/helpers.sh \
+    vendor/du/prebuilt/etc/sysctl.conf:system/etc/sysctl.conf \
+    vendor/du/prebuilt/etc/init.d.cfg:system/etc/init.d.cfg
 
 # userinit support
 PRODUCT_COPY_FILES += \
@@ -78,8 +91,8 @@ PRODUCT_COPY_FILES += \
     
 # HFM Files
 PRODUCT_COPY_FILES += \
-	vendor/du/prebuilt/etc/hosts.alt:system/etc/hosts.alt \
-	vendor/du/prebuilt/etc/hosts.og:system/etc/hosts.og
+    vendor/du/prebuilt/etc/hosts.alt:system/etc/hosts.alt \
+    vendor/du/prebuilt/etc/hosts.og:system/etc/hosts.og
 
 #DUStats
 PRODUCT_PROPERTY_OVERRIDES += \
