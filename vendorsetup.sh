d99db3f572189378446b192ae94d88e1c13295cb
Alex <mazdarider23@gmail.com>
2014-03-06 01:19:27 -0500

    Add Oppo N1 to the mix
    Change-Id: I5029a336dd7b1e100f35a41da0d04aa7d81c4021

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,6 +15,7 @@ add_lunch_combo du_m7ul-userdebug
 add_lunch_combo du_maguro-userdebug
 add_lunch_combo du_mako-userdebug
 add_lunch_combo du_manta-userdebug
+add_lunch_combo du_n1-userdebug
 add_lunch_combo du_n7000-userdebug
 add_lunch_combo du_p3110-userdebug
 add_lunch_combo du_t769-userdebug
