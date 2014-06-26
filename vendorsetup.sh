ea119d370942a46994e1d4db505c4d28e4f567df
Gijs W <thisisnimrr@gmail.com>
2014-06-26 08:55:39 +0200

    Bringup i9100
    Change-Id: Ifba06c7fe2b100201c29d4571ba963852775584f

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,6 +11,7 @@ add_lunch_combo du_gee-userdebug
 add_lunch_combo du_grouper-userdebug
 add_lunch_combo du_hammerhead-userdebug
 add_lunch_combo du_jflte-userdebug
+add_lunch_combo du_i9100-userdebug
 add_lunch_combo du_i9300-userdebug
 add_lunch_combo du_i9305-userdebug
 add_lunch_combo du_i605-userdebug
