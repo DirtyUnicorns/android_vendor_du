48fffc94fcdafabd15938463620dc75489cf31e8
nasty007 <lazic.josip@gmail.com>
2014-08-03 15:23:06 +0200

    add toro
    Change-Id: I5be306e216d6ebc835573555e3fc09f5d319097c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -33,6 +33,7 @@ add_lunch_combo du_t769-userdebug
 add_lunch_combo du_t0lteatt-userdebug
 add_lunch_combo du_t0ltetmo-userdebug
 add_lunch_combo du_taoshan-userdebug
+add_lunch_combo du_toro-userdebug
 add_lunch_combo du_toroplus-userdebug
 add_lunch_combo du_skyrocket-userdebug
 add_lunch_combo du_quincyatt-userdebug
