1cf2df2dcdad893845f6058df8420028d3ebe7d2
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-26 10:33:02 -0800

    ADB over network [2/3]
    Allow SystemServer to set service.adb.tcp.* properties
    Required for network adb enable/disable to function
    
    sepolicy: Fix permissions for service.adb.tcp.port
    This makes the rule more specific by overriding the upstream sepolicy.
    Also adds the adbd context which is necessary for "adb tcpip".
    
    SELinux: Use custom ADB over network property
    * Use a custom system property to trigger the real one, so we avoid
      running afoul of any SELinux CTS requirements.
    
    This makes the rule more specific by overriding the upstream sepolicy.
    Also adds the adbd context which is necessary for "adb tcpip".
    
    Change-Id: Ie5636390ee34a99090b5fa4ca2123019044c7c84

--- config/common.mk
+++ config/common.mk
@@ -8,6 +8,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/bin/sysinit:system/bin/sysinit \
+    vendor/du/prebuilt/etc/init.du.rc:root/init.du.rc
+
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     dalvik.vm.debug.alloc=0 \
