2d0061a53cdd0e300aa20ea59c4e749d01944e86
Josh Chasky <jchasky@gmail.com>
2014-12-14 08:57:11 -0500

    Add Manta to vendorsetup
    Change-Id: Id845449053231643caa5af277fff252cc78f3bba

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,4 +3,5 @@ add_lunch_combo du_flo-userdebug
 add_lunch_combo du_grouper-userdebug
 add_lunch_combo du_hammerhead-userdebug
 add_lunch_combo du_mako-userdebug
+add_lunch_combo du_manta-userdebug
 add_lunch_combo du_shamu-userdebug
