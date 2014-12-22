6c9d0625ea819c74cf8da774a8bc57cc0091333d
Alex Cruz <mazdarider23@gmail.com>
2014-12-22 17:42:17 -0500

    Update vendorsetup.sh to show only caf based devices
    Change-Id: I00cac14d9a64ee750c8c9bcebca3168f7e2bc8d1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,1 @@
-add_lunch_combo du_deb-userdebug
-add_lunch_combo du_flo-userdebug
-add_lunch_combo du_grouper-userdebug
-add_lunch_combo du_hammerhead-userdebug
-add_lunch_combo du_mako-userdebug
-add_lunch_combo du_manta-userdebug
-add_lunch_combo du_shamu-userdebug
+add_lunch_combo du_m7-userdebug
