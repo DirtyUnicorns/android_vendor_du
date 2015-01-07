9ad1c42fb477433b23cf64262eebd513684284f2
moepda <moepda@gmail.com>
2015-01-07 00:05:47 +0000

    Add all supported hlte variants to the mix
    Change-Id: I89290fc72590a022fd2e83ce929ba31fe38ce33e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,8 @@
 add_lunch_combo du_falcon-userdebug
+add_lunch_combo du_hlte-userdebug
+add_lunch_combo du_hltespr-userdebug
+add_lunch_combo du_hltetmo-userdebug
+add_lunch_combo du_hlteusc-userdebug
+add_lunch_combo du_hltevzw-userdebug
 add_lunch_combo du_m7-userdebug
 add_lunch_combo du_moto_msm8960-userdebug
