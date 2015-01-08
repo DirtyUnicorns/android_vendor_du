8306bb17e507e5088f8def87aa73d4f4be3e37f4
Alex Cruz <mazdarider23@gmail.com>
2015-01-08 01:10:37 -0500

    Add the jewel to the mix
    Change-Id: I639285deeeb66ed6acf4e5a284228cb63a845c2f

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,7 @@ add_lunch_combo du_hltespr-userdebug
 add_lunch_combo du_hltetmo-userdebug
 add_lunch_combo du_hlteusc-userdebug
 add_lunch_combo du_hltevzw-userdebug
+add_lunch_combo du_jewel-userdebug
 add_lunch_combo du_jfltetmo-userdebug
 add_lunch_combo du_m7-userdebug
 add_lunch_combo du_moto_msm8960-userdebug
