e7ca1bd6c216b2c22e4fc27f6f0df1a3d58ca6df
Alex Cruz <mazdarider23@gmail.com>
2015-01-08 05:33:20 -0500

    Add version and sprint m7's to the mix
    Change-Id: Iee3e4a6bc8a1aec3744ec0fb1f8fa391685f0847

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,4 +7,6 @@ add_lunch_combo du_hltevzw-userdebug
 add_lunch_combo du_jewel-userdebug
 add_lunch_combo du_jfltetmo-userdebug
 add_lunch_combo du_m7-userdebug
+add_lunch_combo du_m7spr-userdebug
+add_lunch_combo du_m7vzw-userdebug
 add_lunch_combo du_moto_msm8960-userdebug
