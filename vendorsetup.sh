8784ad057a2480d2da9703d96b0d9dfeb2d4a2ba
moepda <moepda@gmail.com>
2015-05-27 22:52:56 +0100

    Remove hlteusc as no longer supported
    Change-Id: I18029f81cc2d3dbf0939069952f44aa6c6b9d32b

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,6 @@ add_lunch_combo du_find7-userdebug
 add_lunch_combo du_hlte-userdebug
 add_lunch_combo du_hltespr-userdebug
 add_lunch_combo du_hltetmo-userdebug
-add_lunch_combo du_hlteusc-userdebug
 add_lunch_combo du_hltevzw-userdebug
 add_lunch_combo du_jewel-userdebug
 add_lunch_combo du_jflteatt-userdebug
