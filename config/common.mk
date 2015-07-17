c0d904b68a5eddfdd151682da1e82cbecff05108
Jason <leonardjason90.jl@gmail.com>
2015-07-17 19:38:10 -0400

    Fix boot animation size issue.
    Change-Id: I211957db037c384c0ea60c2f36d766562c8e190e

--- config/common.mk
+++ config/common.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/du/overlay/common
 
-ifeq ($(ZARACL_BOOTANIMATION),)
+ifeq ($(ZARACL_BOOTANIMATION),true)
 # Boot Animation
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/media/zaracl_bootanimation.zip:system/media/bootanimation.zip
