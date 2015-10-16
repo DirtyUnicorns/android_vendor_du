c4548343a9dea748846c3c33aa5df5a5bb68816f
sspencer10 <sspencer10@gmail.com>
2015-10-16 09:56:26 -0500

    You forgot soMething..
    Change-Id: I9d727f296e3b4876674140857ea06032280bb7f8

--- config/common.mk
+++ config/common.mk
@@ -151,7 +151,7 @@ DU_MOD_VERSION := DU_$(DU_BUILD)_$(ANDROID_VERSION)_$(shell date -u +%Y%m%d-%H%M
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
     ro.du.version=$(DU_VERSION) \
-    ro.mod.version=$(DU_BUILD_TYPE)-v9.7 \
+    ro.mod.version=$(DU_BUILD_TYPE)-v10.0 \
 
 #Build DU-Updater only if DU_BUILD_TYPE isn't DIRTY-DEEDS
 ifneq ($(DU_BUILD_TYPE),DIRTY-DEEDS)
