dfdad373dca2fcaf3caea278c49dac4a9ca3f8f2
Susan <smac0628@gmail.com>
2015-11-11 20:47:36 -0500

    Add Angler to the mix.
    Change-Id: Idcc09a71ca610059069670a8736570b9e95e070d

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,3 +5,4 @@ add_lunch_combo du_hammerhead-userdebug
 add_lunch_combo du_mako-userdebug
 add_lunch_combo du_manta-userdebug
 add_lunch_combo du_shamu-userdebug
+add_lunch_combo du_angler-userdebug
