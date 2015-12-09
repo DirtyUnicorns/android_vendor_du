15e7056eeaf00f0d2dbf60b234b15c149c8aa4b8
Josh Chasky <jchasky@gmail.com>
2015-12-09 15:05:48 -0500

    Remove unsupported devices
    Change-Id: I7f3a828540e45d8c1add503d90cb5a0d555e865d

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,8 +2,6 @@ add_lunch_combo du_angler-userdebug
 add_lunch_combo du_bullhead-userdebug
 add_lunch_combo du_flo-userdebug
 add_lunch_combo du_flounder-userdebug
-add_lunch_combo du_grouper-userdebug
 add_lunch_combo du_hammerhead-userdebug
 add_lunch_combo du_mako-userdebug
-add_lunch_combo du_manta-userdebug
 add_lunch_combo du_shamu-userdebug
