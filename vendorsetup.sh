e9dc4111d226aceced2d7448d17fcf894ce423c2
Josh <joshcorse@gmail.com>
2015-12-18 20:40:34 -0500

    add vk810 to the mix remove unsupported devices
    Change-Id: Ifd554519d334bc88fbcc47813f74d9a9090bfe8c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,5 @@
 add_lunch_combo du_bacon-userdebug
-add_lunch_combo du_cancro-userdebug
 add_lunch_combo du_dlx-userdebug
-add_lunch_combo du_falcon-userdebug
-add_lunch_combo du_find7-userdebug
-add_lunch_combo du_hlte-userdebug
-add_lunch_combo du_hltespr-userdebug
-add_lunch_combo du_hltetmo-userdebug
-add_lunch_combo du_hltevzw-userdebug
 add_lunch_combo du_jewel-userdebug
 add_lunch_combo du_jflteatt-userdebug
 add_lunch_combo du_jfltecan-userdebug
@@ -15,16 +8,10 @@ add_lunch_combo du_jfltetmo-userdebug
 add_lunch_combo du_jflteusc-userdebug
 add_lunch_combo du_jfltevzw-userdebug
 add_lunch_combo du_jfltexx-userdebug
-add_lunch_combo du_klte-userdebug
-add_lunch_combo du_ks01lte-userdebug
 add_lunch_combo du_m7-userdebug
 add_lunch_combo du_m7spr-userdebug
 add_lunch_combo du_m7vzw-userdebug
 add_lunch_combo du_m8-userdebug
 add_lunch_combo du_obake-userdebug
-add_lunch_combo du_trltespr-userdebug
-add_lunch_combo du_trltetmo-userdebug
-add_lunch_combo du_trltexx-userdebug
-add_lunch_combo du_xt926-userdebug
-add_lunch_combo du_yuga-userdebug
-add_lunch_combo du_zaracl-userdebug
+add_lunch_combo du_vk810-userdebug
+
