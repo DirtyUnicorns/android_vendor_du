917f0ee2e594aeb94f5505e9089f6c27275398e2
wrapped <ddroidx@gmail.com>
2016-01-19 20:27:23 -0600

    Add Shieldtablet
    Change-Id: I917329eefbaad5806059d93afea65a9a91be5d9a

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,3 +5,4 @@ add_lunch_combo du_flounder-userdebug
 add_lunch_combo du_hammerhead-userdebug
 add_lunch_combo du_mako-userdebug
 add_lunch_combo du_shamu-userdebug
+add_lunch_combo du_shieldtablet-userdebug
