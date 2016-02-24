08e9ddb2281ae33089029debfbf4d1c5ad61ada6
jbats <jbatscmb@gmail.com>
2016-02-24 00:16:09 -0700

    add Clark to the lineup
    - Remove obake and jewel from the menu for now
    
    Change-Id: Iabb030253b87c512e012438d12841e8b23fd5c88

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,8 @@
 add_lunch_combo du_bacon-userdebug
+add_lunch_combo du_clark-userdebug
 add_lunch_combo du_dlx-userdebug
 add_lunch_combo du_find7-userdebug
-add_lunch_combo du_jewel-userdebug
+#add_lunch_combo du_jewel-userdebug
 add_lunch_combo du_jflteatt-userdebug
 add_lunch_combo du_jfltecan-userdebug
 add_lunch_combo du_jfltespr-userdebug
@@ -13,6 +14,6 @@ add_lunch_combo du_m7-userdebug
 add_lunch_combo du_m7spr-userdebug
 add_lunch_combo du_m7vzw-userdebug
 add_lunch_combo du_m8-userdebug
-add_lunch_combo du_obake-userdebug
+#add_lunch_combo du_obake-userdebug
 add_lunch_combo du_r7plus-userdebug
 add_lunch_combo du_vk810-userdebug
