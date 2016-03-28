c7007937fcf8d2ffaa14639afb5ffaa7a414c7e5
Alex Cruz <mazdarider23@gmail.com>
2016-03-28 09:12:04 -0400

    Use separate vendorsetup.sh for CAF devices [1/2]
    Although we're using the same vendor for both branches, I can already hear
    source builders that don't exactly use common sense, complaining about how they
    can't compile the bacon on the m branch or they can't compile the angler on the
    m-caf branch. This protects the dev from the dev :-)
    
    Change-Id: I734fcce40dd82a89b93e48dd690a834a94b732ce

---
+++ caf-vendorsetup.sh
@@ -0,0 +1,5 @@
+add_lunch_combo du_bacon-userdebug
+add_lunch_combo du_clark-userdebug
+add_lunch_combo du_dlx-userdebug
+add_lunch_combo du_d855-userdebug
+add_lunch_combo du_m8-userdebug
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,8 @@
 add_lunch_combo du_angler-userdebug
-add_lunch_combo du_bacon-userdebug
 add_lunch_combo du_bullhead-userdebug
-add_lunch_combo du_clark-userdebug
-add_lunch_combo du_dlx-userdebug
-add_lunch_combo du_d855-userdebug
 add_lunch_combo du_flo-userdebug
 add_lunch_combo du_flounder-userdebug
 add_lunch_combo du_hammerhead-userdebug
 add_lunch_combo du_mako-userdebug
-add_lunch_combo du_m8-userdebug
 add_lunch_combo du_shamu-userdebug
 add_lunch_combo du_shieldtablet-userdebug
