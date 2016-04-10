764bf68dd8e9a341f87d29d4f31d9ca0b6532a0b
wrapped <ddroidx@gmail.com>
2016-04-09 21:42:14 -0500

    Add back m7 and m7vzw, forget m7spr
    Change-Id: I9a1b90bfaf20a49a8df26fe315b1f13df9072929

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -2,5 +2,7 @@ add_lunch_combo du_bacon-userdebug
 add_lunch_combo du_clark-userdebug
 add_lunch_combo du_dlx-userdebug
 add_lunch_combo du_d855-userdebug
+add_lunch_combo du_m7-userdebug
+add_lunch_combo du_m7vzw-userdebug
 add_lunch_combo du_m8-userdebug
 add_lunch_combo du_r7plus-userdebug
