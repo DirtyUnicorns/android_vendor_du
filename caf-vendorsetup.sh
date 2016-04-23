745f7d935df142a133549ff1340a621cf6e1bc29
nickdoh81 <nickdoh81@gmail.com>
2016-04-23 00:03:29 -0400

    Add Lux to the mix
    - Remove the OnePlus One
    
    Change-Id: I927fc14d88b5d2c96fd7f222c53ccc4864b53698

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -1,7 +1,7 @@
-add_lunch_combo du_bacon-userdebug
 add_lunch_combo du_clark-userdebug
 add_lunch_combo du_dlx-userdebug
 add_lunch_combo du_d855-userdebug
+add_lunch_combo du_lux-userdebug
 add_lunch_combo du_m7-userdebug
 add_lunch_combo du_m7vzw-userdebug
 add_lunch_combo du_m8-userdebug
