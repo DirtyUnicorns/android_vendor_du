69c81288b3f93afff6ab877065e013830693d0a9
anders3408 <anders3408@yahoo.dk>
2016-07-14 20:41:49 +0200

    Add support for find7s
    Change-Id: Ib016eae3f715612c88199f2dc16a3c76a9cf7743

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -1,6 +1,7 @@
 add_lunch_combo du_clark-userdebug
 add_lunch_combo du_dlx-userdebug
 add_lunch_combo du_d855-userdebug
+add_lunch_combo du_find7s-userdebug
 add_lunch_combo du_lux-userdebug
 add_lunch_combo du_m7-userdebug
 add_lunch_combo du_m7vzw-userdebug
