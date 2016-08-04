a4b90a4dc9776b7f1cab7709c00745a98b431156
Alex Cruz <mazdarider23@gmail.com>
2016-08-03 22:23:26 -0400

    D855 is no longer supported
    Change-Id: I6397c65786ff25c149fffb7a0e37154f5a92c2bf

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -1,7 +1,6 @@
 add_lunch_combo du_bacon-userdebug
 add_lunch_combo du_clark-userdebug
 add_lunch_combo du_dlx-userdebug
-add_lunch_combo du_d855-userdebug
 add_lunch_combo du_find7s-userdebug
 add_lunch_combo du_lux-userdebug
 add_lunch_combo du_m7-userdebug
