e59e99924c3ba782ce82225a52b532e3bb8a6cd8
Josh Chasky <jchasky@gmail.com>
2016-08-15 19:29:12 -0400

    Add ether into the fray!
    PUMP UP THE VOLUME!
    
    Change-Id: If71c1a153c9b12078248cb87c28807c1392e2ada

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -1,6 +1,7 @@
 add_lunch_combo du_bacon-userdebug
 add_lunch_combo du_clark-userdebug
 add_lunch_combo du_dlx-userdebug
+add_lunch_combo du_ether-userdebug
 add_lunch_combo du_find7s-userdebug
 add_lunch_combo du_lux-userdebug
 add_lunch_combo du_m7-userdebug
