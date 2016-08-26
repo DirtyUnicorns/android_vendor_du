8c42a16fc62355a6d4b557cf976461a4234c81b6
Josh Chasky <jchasky@gmail.com>
2016-08-26 07:59:45 -0400

    Remove Mako
    Change-Id: I13f25d0367bbe57f2b2d918bfff32fd281bfe122

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,6 +3,5 @@ add_lunch_combo du_bullhead-userdebug
 add_lunch_combo du_dragon-userdebug
 add_lunch_combo du_flounder-userdebug
 add_lunch_combo du_hammerhead-userdebug
-add_lunch_combo du_mako-userdebug
 add_lunch_combo du_shamu-userdebug
 add_lunch_combo du_shieldtablet-userdebug
