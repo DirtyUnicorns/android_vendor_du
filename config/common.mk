bb5d0db58019c65c81f217fd13a9a393abaffbce
Josh Chasky <jchasky@gmail.com>
2016-09-08 19:39:41 -0400

    Allow AOSP KB to gesture type
    - Add condition for 32/64 devices
    
    Change-Id: Ia19043df237f41ebe0ff556fe794eb115d6ec212

--- config/common.mk
+++ config/common.mk
@@ -28,6 +28,17 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/etc/init.local.rc:root/init.du.rc
 
+# LatinIME gesture typing
+ifeq ($(TARGET_ARCH),arm64)
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/du/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+    vendor/du/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+endif
+
 # Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/etc/mkshrc:system/etc/mkshrc
