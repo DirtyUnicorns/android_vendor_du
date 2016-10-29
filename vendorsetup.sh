1a712f7f750559a8649ddf37f0dce58e2916d807
Josh Chasky <jchasky@gmail.com>
2016-10-29 11:16:41 -0400

    Remove Flounder from official support.. for now
    Change-Id: I70f86cbe5d67c9eb31d01865e634e4dd7c2d2d5c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,6 @@
 add_lunch_combo du_angler-userdebug
 add_lunch_combo du_bullhead-userdebug
 add_lunch_combo du_dragon-userdebug
-add_lunch_combo du_flounder-userdebug
 add_lunch_combo du_hammerhead-userdebug
 add_lunch_combo du_shamu-userdebug
 add_lunch_combo du_shieldtablet-userdebug
