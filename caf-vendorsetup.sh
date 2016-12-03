899bf03e9093f1a3be12ec0fe252bcc0981b1e86
Josh Chasky <jchasky@gmail.com>
2016-12-03 07:33:07 -0500

    Remove currently unsupported devices
    Change-Id: I5c3c2c834a583b35672637eb3d249cbc96ad9fc3

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -1,8 +1,5 @@
-add_lunch_combo du_bacon-userdebug
 add_lunch_combo du_clark-userdebug
-add_lunch_combo du_ether-userdebug
 add_lunch_combo du_h815-userdebug
 add_lunch_combo du_lux-userdebug
-add_lunch_combo du_m8-userdebug
 add_lunch_combo du_oneplus2-userdebug
 add_lunch_combo du_oneplus3-userdebug
