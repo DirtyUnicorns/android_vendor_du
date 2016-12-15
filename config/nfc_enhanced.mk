bc6f191060bebf5953426ca7325c8269b002d0fa
Alex Cruz <mazdarider23@gmail.com>
2016-12-15 17:26:06 +0100

    Clean up vendor
    - Remove DU Certified
    - Remove old CMTE permissions
    - No longer use gsm/cdma or nfc enhanced make files
    - Clean up sepolicy (mainly CMTE stuff)
    
    Change-Id: I8b321adfd931f297355df756895e338721af8914

--- config/nfc_enhanced.mk
+++
@@ -1,3 +0,0 @@
-# This device supports enhanced NFC
-PRODUCT_COPY_FILES += \
-    vendor/du/config/permissions/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml
--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,7 +1,2 @@
-#
-# This policy configuration will be used by all products that
-# inherit from CM
-#
-
 BOARD_SEPOLICY_DIRS += \
     vendor/du/sepolicy
