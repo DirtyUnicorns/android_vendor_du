1030100e300b6a27933e22737e2d20ec72dcdfed
Alex Cruz <mazdarider23@gmail.com>
2016-12-23 08:49:28 +0100

    Remove OP3 for now
    Change-Id: Ia5849794e80aa78be64e992f3b5065b060041468

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -2,5 +2,4 @@ add_lunch_combo du_clark-userdebug
 add_lunch_combo du_h815-userdebug
 add_lunch_combo du_lux-userdebug
 add_lunch_combo du_oneplus2-userdebug
-add_lunch_combo du_oneplus3-userdebug
 add_lunch_combo du_osprey-userdebug
