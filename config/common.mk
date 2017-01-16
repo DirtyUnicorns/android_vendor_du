a85af77783c43434e4d871efae31f152e5d18d66
spaceman860 <spaceman8583@gmail.com>
2017-01-16 15:35:13 -0600

    Remove SU system dingleberry
    Change-Id: I798fb0ad292279588142ec02d1d7c0149338c383

--- config/common.mk
+++ config/common.mk
@@ -88,8 +88,7 @@ PRODUCT_COPY_FILES += \
 
 # SuperSU
 PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
-    vendor/du/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+    vendor/du/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip
 
 # Versioning System
 ANDROID_VERSION = 7.1.1
