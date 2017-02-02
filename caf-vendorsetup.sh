7a783afdb0b4a51c8190803e7faa706b2680da07
Nick Doherty <nickdoh81@gmail.com>
2017-02-02 08:09:30 -0500

    Revert "Remove OP3 and PME till issues are cleared"
    This reverts commit 9168c826b824aa7efb09efccbbc7bb1970c3ebbe.
    
    Change-Id: If5c280f4fb18fd37df1022270891328bfdb46c32

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -2,4 +2,6 @@ add_lunch_combo du_clark-userdebug
 add_lunch_combo du_h815-userdebug
 add_lunch_combo du_lux-userdebug
 add_lunch_combo du_oneplus2-userdebug
+add_lunch_combo du_oneplus3-userdebug
 add_lunch_combo du_osprey-userdebug
+add_lunch_combo du_pme-userdebug
