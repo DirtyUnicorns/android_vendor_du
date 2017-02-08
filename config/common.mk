707cf10bf410ec461b17f293fea9c462d8f695e4
moepda <moepda@gmail.com>
2017-02-08 23:51:17 +0000

    Revert "Enable assistant on all devices"
    Unfortunately this doesnt work on all devices.
    
    This reverts commit 225c4880b2a8f9bca4dee0fb1d13c3436f63197b.
    
    Change-Id: I0be50b374bdf20f4dd21cc0f07bab29e6c298f32

--- config/common.mk
+++ config/common.mk
@@ -20,8 +20,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.build.selinux=1 \
-    ro.com.android.dataroaming=false \
-    ro.opa.eligible_device=true
+    ro.com.android.dataroaming=false
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
