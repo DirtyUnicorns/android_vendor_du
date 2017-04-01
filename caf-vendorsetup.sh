8118e66a4c6ded472b29d045bcf1d862a6734124
Josh Chasky <jchasky@gmail.com>
2017-04-01 07:42:41 -0400

    Remove h815 for now
    Change-Id: I6ee41becd61e9ce8a0aba1ef2f4dbf92cfb6a569

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -1,7 +1,6 @@
 add_lunch_combo du_clark-userdebug
 add_lunch_combo du_d802-userdebug
 add_lunch_combo du_ether-userdebug
-add_lunch_combo du_h815-userdebug
 add_lunch_combo du_lux-userdebug
 add_lunch_combo du_oneplus2-userdebug
 add_lunch_combo du_oneplus3-userdebug
