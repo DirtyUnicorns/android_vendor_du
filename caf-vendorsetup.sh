223240fb93e5bbfc10aa0d78af83072cfbf48def
Josh Chasky <jchasky@gmail.com>
2017-04-19 16:24:55 -0400

    Another one bites the dust..
    Unfortunately, my Robin fell and broke, so now I have a non-functioning display.
    Removing it from vendorsetup until I can get it fixed or replaced.
    I'm sure this will disappoint some, but this is just the way it is.. for now, anyway.
    
    Change-Id: Ia0131b776e2709f67489cc7f629fa2d14cdea94e

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -1,6 +1,5 @@
 add_lunch_combo du_clark-userdebug
 add_lunch_combo du_d802-userdebug
-add_lunch_combo du_ether-userdebug
 add_lunch_combo du_lux-userdebug
 add_lunch_combo du_oneplus2-userdebug
 add_lunch_combo du_oneplus3-userdebug
