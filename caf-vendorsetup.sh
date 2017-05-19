7b93fd9bbaf6630aae9690cd957b561f8af4ab2f
Sean Hoyt <seanhoyt963@gmail.com>
2017-05-19 00:42:45 -0400

    Enable x2 for weeklies
    Change-Id: I56444d3250cabfc746d195afdb3409472fdc28cd

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -6,3 +6,4 @@ add_lunch_combo du_oneplus3-userdebug
 add_lunch_combo du_osprey-userdebug
 add_lunch_combo du_pme-userdebug
 add_lunch_combo du_shieldtablet-userdebug
+add_lunch_combo du_x2-userdebug
