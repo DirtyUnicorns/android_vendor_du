12afd33f78c7fc008c37aa88e959742f14c4339b
Josh Chasky <jchasky@gmail.com>
2017-06-22 13:44:32 -0400

    Remove OP2 from official support
    Change-Id: Idcc0164ae8f505a1ba53e2aa18b31aa7416c627c

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -1,7 +1,6 @@
 add_lunch_combo du_clark-userdebug
 add_lunch_combo du_d802-userdebug
 add_lunch_combo du_lux-userdebug
-add_lunch_combo du_oneplus2-userdebug
 add_lunch_combo du_oneplus3-userdebug
 add_lunch_combo du_osprey-userdebug
 add_lunch_combo du_pme-userdebug
