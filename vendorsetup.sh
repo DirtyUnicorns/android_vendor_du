f7f02086f4daf44727a13276ff02bd91af8ca99e
Wrapped <ddroidx@gmail.com>
2017-07-13 21:47:55 -0500

    Move Shieldtablet to the n7x branch
    Change-Id: I0e51797384608bacc286ed8b3d371ecca5a60181

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,3 +5,4 @@ add_lunch_combo du_hammerhead-userdebug
 add_lunch_combo du_marlin-userdebug
 add_lunch_combo du_sailfish-userdebug
 add_lunch_combo du_shamu-userdebug
+add_lunch_combo du_shieldtablet-userdebug
