fe6ba31b3944ccee8a93977dd08fc5e5e11d98a8
Nathan Chancellor <natechancellor@gmail.com>
2017-08-27 21:06:26 +0200

    Clean up and slim down
    1. PLATFORM_VERSION_CODENAME won't be used after http://gerrit.dirtyunicorns.com/#/c/6642/
    2. No need to define ANDROID_VERSION when PLATFORM_VERSION already exists
    3. Add DU_BASE_VERSION so mod and full version work
    4. Remove DU_MOD_VERSION, it isn't used anywhere
    
    Change-Id: Ie0b7a298e1d60433fae631289e29ed0a043b31be

--- config/branding.mk
+++ config/branding.mk
@@ -1,9 +1,8 @@
 # Versioning System
-ANDROID_VERSION = 7.1.2
-DU_VERSION = v11.7
+DU_BASE_VERSION = v11.7
+
 ifndef DU_BUILD_TYPE
     DU_BUILD_TYPE := UNOFFICIAL
-    PLATFORM_VERSION_CODENAME := UNOFFICIAL
 endif
 
 # Only include DU-Updater for official, weeklies, and rc builds
@@ -18,11 +17,9 @@ ifeq ($(filter-out OFFICIAL WEEKLIES,$(DU_BUILD_TYPE)),)
 endif
 
 # Set all versions
-DU_VERSION := DU_$(DU_BUILD)_$(ANDROID_VERSION)_$(shell date -u +%Y%m%d-%H%M).$(DU_VERSION)-$(DU_BUILD_TYPE)
-DU_MOD_VERSION := DU_$(DU_BUILD)_$(ANDROID_VERSION)_$(shell date -u +%Y%m%d-%H%M).$(DU_VERSION)-$(DU_BUILD_TYPE)
+DU_VERSION := DU_$(DU_BUILD)_$(PLATFORM_VERSION)_$(shell date -u +%Y%m%d-%H%M).$(DU_BASE_VERSION)-$(DU_BUILD_TYPE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
     ro.du.version=$(DU_VERSION) \
-    ro.mod.version=$(DU_BUILD_TYPE)-v11.7
-
+    ro.mod.version=$(DU_BUILD_TYPE)-$(DU_BASE_VERSION)
