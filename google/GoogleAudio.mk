DirtyUnicorns/android_vendor_du

commit 56400a3233cd46c352e753a6a343164ea43350b7

Set new pixel sounds as defaults
Change-Id: I9f60a982e2d103fdb5b0e414d82099288dce76f7

google/GoogleAudio.mk
@@ -98,3 +98,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ringtones/Triton.ogg:system/media/audio/ringtones/Triton.ogg \
     $(LOCAL_PATH)/ringtones/Umbriel.ogg:system/media/audio/ringtones/Umbriel.ogg \
     $(LOCAL_PATH)/ringtones/Zen.ogg:system/media/audio/ringtones/Zen.ogg
+
+PRODUCT_PROPERTY_OVERRIDES := \
+   ro.config.ringtone=Zen.ogg \
+   ro.config.notification_sound=Chime.ogg

config/common.mk
@@ -51,3 +51,6 @@ include vendor/du/config/branding.mk
 
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/du/overlay/common
+
+# Google sounds
+include vendor/du/google/GoogleAudio.mk

commit ade1cbc7f7888e86852a5461121dfaeb13ada508

vendor: Fix PRODUCT_PROPERTY_OVERRIDES
This file gets included in config/common.mk, where we have other
property values set (such as ro.build.selinux for the status in the
About phone section). The problem with ":=" is it will override the
previous value with the new one, where as "+=" will add the supplied
value on to the existing one. With this change, all property values
are properly added!

Change-Id: Ibc32d1b8155bcd391dd95ab8b71d984c0dd54a99
Fixes: 56400a32 ("Set new pixel sounds as defaults")
Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

google/GoogleAudio.mk
@@ -99,6 +99,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ringtones/Umbriel.ogg:system/media/audio/ringtones/Umbriel.ogg \
     $(LOCAL_PATH)/ringtones/Zen.ogg:system/media/audio/ringtones/Zen.ogg
 
-PRODUCT_PROPERTY_OVERRIDES := \
+PRODUCT_PROPERTY_OVERRIDES += \
    ro.config.ringtone=Zen.ogg \
    ro.config.notification_sound=Chime.ogg