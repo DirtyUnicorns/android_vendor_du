103704297a4e2c06964ce6514d57eedce01b480c
jbats <jbatscmb@gmail.com>
2018-03-20 20:09:47 -0600

    add nash to the mix
    Change-Id: I99ef5c0fbc1241301b86cd380825746230771b7b

--- caf-vendorsetup.sh
+++ caf-vendorsetup.sh
@@ -2,6 +2,8 @@ add_lunch_combo du_cheeseburger-user
 add_lunch_combo du_cheeseburger-userdebug
 add_lunch_combo du_dumpling-user
 add_lunch_combo du_dumpling-userdebug
+add_lunch_combo du_nash-user
+add_lunch_combo du_nash-userdebug
 add_lunch_combo du_oneplus3-user
 add_lunch_combo du_oneplus3-userdebug
 add_lunch_combo du_potter-user
