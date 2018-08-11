7bbb647231c1efaec64bd007f51edb1c2e0aab37
Nick Reuter <nreuter85@gmail.com>
2018-08-10 20:53:30 -0500

    update vendorsetup to list all aosp/caf devices
     Will cleanup list when we establish what is staying/going
    
    Change-Id: Ie0ecc91d5bd8c68c514fdf74272ce76805fda32d

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,20 @@
-for device in angler dragon flo hammerhead marlin shamu taimen; do
+supported_devices=(
+  'angler'
+  'cheeseburger'
+  'dragon'
+  'dumpling'
+  'flo'
+  'hammerhead'
+  'marlin'
+  'nash'
+  'oneplus3'
+  'potter'
+  'shamu'
+  'taimen'
+  'tenderloin'
+)
+
+for device in ${supported_devices[@]}; do
     add_lunch_combo du_${device}-user
     add_lunch_combo du_${device}-userdebug
 done
