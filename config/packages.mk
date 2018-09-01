74c5559250776ab266023a2d1757b13b9c7c1676
Alex <alex@dirtyunicorns.com>
2018-09-01 02:24:58 -0500

    Remove extras tools
    Most of these tools are already set to compile in the build repo
    under the 'core minimal' package which is inherited by the base
    
    https://github.com/DirtyUnicorns/android_build/blob/p9x/target/product/core_base.mk#L64
    
    https://github.com/DirtyUnicorns/android_build/blob/p9x/target/product/core_minimal.mk
    
    OpenVPN is obsolete and we inherited that from the commit below
    https://github.com/DirtyUnicorns/android_vendor_du/commit/978aad6a8274544235738e22272bb0e151680775
    
    Change-Id: I6145e35587d059fdfe1138436c57fb5cb5110547

--- config/packages.mk
+++ config/packages.mk
@@ -2,13 +2,6 @@
 PRODUCT_PACKAGES += \
     WallpaperPicker
 
-# Extra tools
-PRODUCT_PACKAGES += \
-    openvpn \
-    e2fsck \
-    mke2fs \
-    tune2fs
-
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
     librsjni
