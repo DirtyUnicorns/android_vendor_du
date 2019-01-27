28bbf7629295515e04f4236807c0afda5dc8db2d
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2019-01-27 09:21:08 +0000

    Build CustomDoze for all devices
    Now that we have AoD option too, there's no reason
    to not ship custom doze magic to all devices
    
    NB: needs our CustomDoze tree, within:
    - https://github.com/DirtyUnicorns/android_frameworks_base/commit/e1a6a61e7ba918e4c6b891029d6cd603a575aae1
    - https://github.com/DirtyUnicorns/android_packages_apps_Settings/commit/bfca97f7395c1ef79dc837ed916a44f98a5acbac
    - https://github.com/DirtyUnicorns/android_packages_apps_Settings/commit/6ffa7b811c130f77bf71273f72118d048d8075a8
    and:
    <!-- Doze: does this device support STATE_DOZE and STATE_DOZE_SUSPEND?  -->
    <bool name="doze_display_state_supported">true</bool>
    (nowaday doze works fine on all modern devices).
    
    Be sure to remove any specific device custom doze package.
    
    PS: to have Ambient brightness changer feature in CustomDoze, be sure to have this too:
    https://github.com/DirtyUnicorns/android_vendor_du/commit/606a3f3d250b3a344bce2cd9b99aff145bb9d4b1
    
    Change-Id: I5e7c9a72d29709cca5c1f109eac1e9239507b4ae

--- config/packages.mk
+++ config/packages.mk
@@ -1,9 +1,9 @@
 # Packages
 PRODUCT_PACKAGES += \
+    CustomDoze \
     WallpaperPicker \
     WeatherClient
 
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
     librsjni
-
