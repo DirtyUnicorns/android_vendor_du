d9d3415655aa7248f9b3f4a4822e884115e9ce1d
Alex Koskovich <zvnexus@outlook.com>
2019-10-18 20:12:39 -0400

    Remove CAF check for vendorsetup.
    - Branches are unified now.
    
    Change-Id: I1384de926d4bf7c8853826bdeac09e78062afd06

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,10 @@
-aosp_devices=('blueline' 'bonito' 'crosshatch' 'dragon' 'marlin' 'sargo' 'taimen')
-caf_devices=('oneplus3')
+devices=('blueline' 'bonito' 'crosshatch' 'dragon' 'marlin' 'sargo' 'taimen')
 
 function lunch_devices() {
     add_lunch_combo du_${device}-user
     add_lunch_combo du_${device}-userdebug
 }
 
-if [[ $( grep -i "caf" manifest/README.md) ]]; then
-    for device in ${caf_devices[@]}; do
-        lunch_devices
-    done
-else
-    for device in ${aosp_devices[@]}; do
-        lunch_devices
-    done
-fi
+for device in ${devices[@]}; do
+    lunch_devices
+done
