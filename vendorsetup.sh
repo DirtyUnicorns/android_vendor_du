22008cf49c5508f320522a295b35b34b67a78197
Josh Chasky <jchasky@gmail.com>
2019-12-18 10:24:10 -0500

    Drop support for Dragon
    Change-Id: Iec4858724ed9fe202a2641f854938e99b25a29a8

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-devices=('blueline' 'bonito' 'coral' 'crosshatch' 'dragon' 'emulator' 'marlin' 'oneplus3' 'potter' 'sargo' 'taimen')
+devices=('blueline' 'bonito' 'coral' 'crosshatch' 'emulator' 'marlin' 'oneplus3' 'potter' 'sargo' 'taimen')
 
 function lunch_devices() {
     add_lunch_combo du_${device}-user
