96ae17d117f39c536cec53e86a2917952a29b233
William Bellavnce <flintman@flintmancomputers.com>
2020-08-26 08:08:08 -0400

    Adding River (Moto G7) to the mix
    Change-Id: I63ee2b656c3887983f90803db0fb71a3b4eba509

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-devices=('blueline' 'bonito' 'coral' 'crosshatch' 'emulator' 'lake' 'marlin' 'potter' 'taimen')
+devices=('blueline' 'bonito' 'coral' 'crosshatch' 'emulator' 'lake' 'marlin' 'potter' 'river' 'taimen')
 
 function lunch_devices() {
     add_lunch_combo du_${device}-user
