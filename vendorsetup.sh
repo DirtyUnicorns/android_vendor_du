df598f228a1b0b2f563fe432675fec110817be9f
A. Andriyadi <65210971+kambium30@users.noreply.github.com>
2020-11-07 11:23:43 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-devices=('blueline' 'bonito' 'coral' 'crosshatch' 'emulator' 'lake' 'lavender' 'marlin' 'potter' 'river' 'sunfish' 'taimen')
+devices=('blueline' 'bonito' 'coral' 'crosshatch' 'emulator' 'lake' 'lavender' 'marlin' 'potter' 'river' 'sunfish' 'taimen' 'santoni')
 
 function lunch_devices() {
     add_lunch_combo du_${device}-user
